13d6d32fde06450644011af6ffaf5cb834ff546c
amigin <amigin@gmail.com>
2024-12-23 10:55:22 +0200

    Minor Fixes

--- components/src/components/env_selector.rs
+++ components/src/components/env_selector.rs
@@ -4,57 +4,12 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn EnvSelector(envs: Vec<Rc<String>>, on_change: EventHandler<String>) -> Element {
-    let mut selected_env = use_signal(|| None::<Rc<String>>);
+    let mut selected_env = use_signal(|| SelectedEnv::new(envs));
 
-    let selected_env_value = selected_env.read().clone();
-    let my_eval = if selected_env_value.is_none() {
-        let my_eval = eval(
-            r#"
-            const env = localStorage.getItem("selectedEnv");
-            if (env) {
-                dioxus.send(env);
-            }else{
-                dioxus.send("");
-            }
-            "#,
-        );
+    let selected_env_read_access = selected_env.read();
 
-        let mut eval_owned = my_eval.to_owned();
-        spawn(async move {
-            let value = eval_owned.recv().await.unwrap();
-
-            match value {
-                serde_json::Value::String(value) => {
-                    on_change.call(value.clone());
-                    *selected_env.write() = Some(value.into());
-                }
-
-                _ => {
-                    panic!("Somehow we got non string value");
-                }
-            }
-        });
-
-        my_eval
-    } else {
-        eval(
-            r#"    while(true){
-               let msg = await dioxus.recv();
-               localStorage.setItem("selectedEnv", msg);
-            }"#,
-        )
-    };
-
-    if selected_env_value.is_none() {
-        return rsx! {
-            div { "Loading selected env..." }
-        };
-    }
-
-    let selected_env_value = selected_env_value.unwrap();
-
-    let items = envs.into_iter().map(|itm| {
-        if selected_env_value.as_str() == itm.as_str() {
+    let items = selected_env_read_access.envs.iter().map(|itm| {
+        if selected_env_read_access.as_str() == itm.as_str() {
             return rsx! {
                 option { selected: true, "{itm}" }
             };
@@ -73,12 +28,52 @@ pub fn EnvSelector(envs: Vec<Rc<String>>, on_change: EventHandler<String>) -> El
                 style: "background-color: #2c2c2c;color: white;border-color: black;",
                 onchange: move |e| {
                     let value = e.value();
-                    let value_spawn = value.clone();
-                    let _ = my_eval.send(value_spawn.into());
+                    selected_env.write().set_value(value.as_str());
                     on_change.call(e.value());
                 },
                 {items}
             }
         }
+    }
+}
+
+pub struct SelectedEnv {
+    envs: Vec<Rc<String>>,
+    value: Rc<String>,
+}
+
+impl SelectedEnv {
+    pub fn new(envs: Vec<Rc<String>>) -> Self {
+        let value = dioxus_utils::js::GlobalAppSettings::get_local_storage()
+            .get("selectedEnv")
+            .unwrap_or_default();
+
+        let index = envs.iter().position(|v| v.as_str() == value.as_str());
+
+        let value = match index {
+            Some(index) => envs.get(index).unwrap().clone(),
+
+            None => {
+                if envs.len() > 0 {
+                    envs.get(0).unwrap().clone()
+                } else {
+                    Rc::new(value)
+                }
+            }
+        };
+
+        Self { value, envs }
+    }
+
+    pub fn as_str(&self) -> &str {
+        self.value.as_str()
+    }
+
+    pub fn set_value(&mut self, value: &str) {
+        dioxus_utils::js::GlobalAppSettings::get_local_storage().set("selectedEnv", value);
+
+        let index = self.envs.iter().position(|v| v.as_str() == value).unwrap();
+
+        self.value = self.envs.get(index).unwrap().clone();
     }
 }
--- components/src/states/mod.rs
+++ components/src/states/mod.rs
@@ -1,4 +1,2 @@
 mod env_list_state;
 pub use env_list_state::*;
-mod component_data_holder;
-pub use component_data_holder::*;
